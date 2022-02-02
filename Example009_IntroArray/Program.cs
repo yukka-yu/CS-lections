c323919953e13ad824000b95e1b2764f8cb80fe0
Sasha <yukka.yu@gmail.com>
2022-02-02 11:15:54 +0300

    Finding maximum in array

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,9 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+              int result = arg1;
+              if(arg2 > result) result = arg2;
+              if(arg3 > result) result = arg3;
+              return result;
+}
+
+int[] array = {11, 21, 31, 41, 15, 61, 17, 18, 19};
