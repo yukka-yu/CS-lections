3cba139897c3a69c7dbe873979471dd19804a65e
Sasha <yukka.yu@gmail.com>
2022-02-14 16:46:26 +0300

    Сделаны ДЗ до пятого семинара

--- Seminar/Seminar 009/Program.cs
+++ Seminar/Seminar 009/Program.cs
@@ -2,3 +2,12 @@
 Console.WriteLine("Введите трёхзначное число");
 int number = int.Parse(Console.ReadLine());
 Console.WriteLine("Последняя цифра введённого числа " + number % 10);
+
+// и то же самое через функцию
+
+int ThirdSign(int arg1)
+{
+              return arg1 % 10;
+}
+
+Console.WriteLine("Последняя цифра введённого числа " + ThirdSign(number));
